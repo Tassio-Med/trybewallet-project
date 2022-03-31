142372e20ceec041bf6b99b92087313b03761f3b
Tassio Med <tassiohfmed@gmail.com>
2022-03-31 15:54:11 -0300

    configuração do redux terminada(eu acho...)

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,4 @@
 // Coloque aqui suas actions
+export const newAction = (state) => ({ type: 'NEW_ACTION', state });
+
+export default newAction;
--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,1 +1,2 @@
 // Esse reducer será responsável por tratar as informações da pessoa usuária
+export default users;
